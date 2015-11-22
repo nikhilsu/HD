fe3b3df6ab8954d86457d73d7a97c8e5695b4cff
nikhilsu <nikhilsu@thoughtworks.com>
2015-11-22 14:19:57 +0530

    Made the hotdog a daemon process

---
+++ startDaemon.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+mv script.sh /usr/local/bin/script.sh
+
+echo -e "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\">
+<dict>
+    <key>Label</key>
+        <string>com.prank.hotdog</string>
+        
+    <key>ThrottleInterval</key>
+        <integer>30</integer>
+    
+    <key>ProgramArguments</key>
+        <array>
+                <string>/usr/local/bin/script.sh</string>
+        </array>
+    
+    <key>StartInterval</key>
+        <integer>30</integer>
+        
+    <key>StandardErrorPath</key>
+        <string>~/IPerror.err</string>
+    
+    <key>StandardOutPath</key>
+        <string>~/IPoutput.out</string>
+    
+    <key>KeepAlive</key>
+        <true/>
+</dict>
+</plist>"|cat>script.plist
+
+chmod +x script.plist
+mv script.plist ~/Library/LaunchDaemons/
+
+launchctl load ~/Library/LaunchDaemons/script_to_update_host_IP.plist
